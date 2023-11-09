f8e83683b64e8565cb5b836969db8bbc9b043418
Jon <ulyger23almaty@gmail.com>
2023-11-09 16:02:00 +0800

    update

--- src/routes/jobs/[slug]/update/getId.js
+++
@@ -1,9 +0,0 @@
-
-
-    export function getJobId() {
-        const id = localStorage.getItem("id")
-        if (id) {
-          return JSON.parse(id)["jobId"]
-        }
-        return resp.id
-      }
